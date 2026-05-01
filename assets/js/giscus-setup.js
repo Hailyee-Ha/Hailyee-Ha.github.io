a64de5ab7704e6a2c92f69000a0d40f519aa0d1d
Hailyee-Ha <Hailyee-Ha@users.noreply.github.com>
2026-05-01 19:37:41 +0000

    Deploying to gh-pages from @ Hailyee-Ha/Hailyee-Ha.github.io@df4ccfc50a213c692d141b7b9572b73d801b1d64 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Hailyee-Ha/Hailyee-Ha.github.io-",
+    "data-repo": "Hailyee-Ha/Hailyee-Ha.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
